9674e16269a8a84c6dc3f321fd546e5c99867c34
Mike Ivanov <mikei@activestate.com>
2011-07-28 17:49:22 -0700

    Reliable timeout implementation

--- lib/vcap/sysadm.rb
+++ lib/vcap/sysadm.rb
@@ -36,15 +36,32 @@ module SA
     system("#{SYSADM} rmtmp #{dir}")
   end
 
-  def SA.run_as (user, command, options={}, &block)
-    { :dir => nil, :env => nil, :limits => nil, :close_stdin => false}.merge! options
+  def SA.lxc (command, *args, &block)
+    exec_operation = proc { |process| process }
+    exit_callback = block || (proc do |o,s| nil end)
+    
+    #XXX: potential vulnerability: unescaped user data.
+    #     it might make sense to send args through stdin
+    EM.system("/bin/sh", "-c", "#{SYSADM} runlxc #{user} #{args.join(' ')} 2>&1",
+              exec_operation, exit_callback)
+  end
+
+  def SA.run_as (user, command, opts={}, &block)
+    opts = {:dir => nil, :env => nil, :limits => nil, :timeout => 0}.merge(opts)
+
+    timer = nil
+    terminate = nil
+    terminated = false
 
     exec_operation = proc do |process|
+      terminate = proc do
+        process.close_connection_after_writing()
+      end
       # set the minimal usable PATH
       process.send_data("export PATH=/usr/bin:/bin\n")
-      process.send_data("cd #{options[:dir]}\n") if options[:dir]
+      process.send_data("cd #{opts[:dir]}\n") if opts[:dir]
       # we want to limit all processes potentially running user code, without exceptions
-      limits = {:mem => ONE_GIG, :fds => 4096, :disk => ONE_GIG}.merge! (options[:limits] || {})
+      limits = {:mem => ONE_GIG, :fds => 4096, :disk => ONE_GIG}.merge! (opts[:limits] || {})
       # ulimit -m takes kb, soft enforce
       process.send_data("ulimit -m #{(limits[:mem]/1024).to_i} 2> /dev/null\n")  
       # virtual memory at 3G, this will be enforced
@@ -54,26 +71,38 @@ module SA
       # File size to complete disk usage
       process.send_data("ulimit -f #{limits[:disk]} 2> /dev/null\n") 
       process.send_data("umask 077\n")
-
       # XXX: value may not contain single quotes; see
       # http://bugs.activestate.com/show_bug.cgi?id=90720#c9
-      (options[:env] || {}).each do |k,v| 
+      (opts[:env] || {}).each do |k,v| 
         export_line = "export #{k}=#{v}\n"
         process.send_data(export_line)
       end
-
-      command = "#{command} < /dev/null" if options[:close_stdin]
-      #XXX: is there any reason they used explicit exit instead of exec?
       process.send_data("exec #{command}\n")
-      #process.send_data("exit\n")
       process
     end
 
-    exit_callback = block || (proc do |o,s| nil end)
+    exit_callback = proc do |output, status|
+      EM.cancel_timer(timer) if timer
+      if terminated
+        output += "\n" if output.length > 0
+        output += "*Terminated by timeout*\n"
+      end
+      block.call(output, status) if block
+    end
     
     # it *must* use the three-arg form, otherwise the stderr output gets lost
-    EM.system("/bin/sh", "-c", "env -i #{SYSADM} runas #{user} 2>&1",
-              exec_operation, exit_callback)
+    pid = EM.system("/bin/sh", "-c", "env -i #{SYSADM} runas #{user} 2>&1",
+                    exec_operation, exit_callback)
+
+    timeout = opts[:timeout] || 0
+    if timeout > 0
+      timer = EM.add_timer(timeout) do
+        timer = nil
+        terminated = true
+        terminate.call
+      end
+    end
+    pid
   end
 
   def SA.instance_pid (instance_dir)
