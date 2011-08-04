498df787bc22d05c512daa03249c8ef893327718
Daniil Kulchenko <daniil@kulchenko.com>
2011-08-04 12:17:24 -0700

    Added LXC support to DEA.

--- lib/vcap/sysadm.rb
+++ lib/vcap/sysadm.rb
@@ -1,6 +1,8 @@
 ONE_GIG = 1024*1024*1024
 SYSADM = "~/stackato/tools/sysadm"
 
+require "yaml"
+
 module SA
   class Error < Exception
   end
@@ -30,6 +32,39 @@ module SA
 
   def SA.remove_tmp_dir (dir)
     sys "#{SYSADM} rmtmp #{dir}"
+  end
+
+  def SA.create_container (id, user) #TODO: add memory and disk limits
+    `#{SYSADM} runlxc create_container #{id} #{user[:user]} #{user[:uid]}`.strip # this returns the full directory
+  end
+
+  def SA.start_container (id)
+    `#{SYSADM} runlxc start_container #{id}"`.strip # this should return the IP of the container
+  end
+
+  def SA.stop_container (id)
+    system("#{SYSADM} runlxc stop_container #{id}")
+  end
+
+  def SA.destroy_container (id)
+    system("#{SYSADM} runlxc destroy_container #{id}")
+  end
+ 
+  def SA.untar (tgz_file, untar_to)
+    system("#{SYSADM} runlxc untar_file #{tgz_file} #{untar_to}")
+  end
+
+  def SA.runlxc (instance_id, user, cmd, env)
+    File.open( "/tmp/#{instance_id}.env", "w" ) { |file| YAML::dump( env, file ) } #XXX: do I *really* have to do this?
+    `#{SYSADM} runlxc runlxc #{instance_id} #{user[:user]} #{user[:uid]}"#{cmd}"`.strip
+  end
+
+  def SA.install_forwarding (port, lxcip, lxcport)
+    system("#{SYSADM} runlxc install_forwarding #{port} #{lxcip} #{lxcport}")
+  end
+
+  def SA.remove_forwarding (port, lxcip, lxcport)
+    system("#{SYSADM} runlxc remove_forwarding #{port} #{lxcip} #{lxcport}")
   end
 
   def SA.lxc (command, *args, &block)
