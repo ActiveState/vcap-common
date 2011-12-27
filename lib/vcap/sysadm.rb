4e6e9e6149a81dcbdf3ee14fa3db6a20a41f1f7d
Daniil Kulchenko <daniil@kulchenko.com>
2011-12-27 10:26:56 -0800

    Remove secure users from Stackato, replaced with LXC (bug #91847).

--- lib/vcap/sysadm.rb
+++ lib/vcap/sysadm.rb
@@ -14,26 +14,6 @@ module SA
     out
   end
 
-  def SA.grant_ownership (user, dir)
-    sys "#{SYSADM} grant #{user} #{dir}"
-  end
-
-  def SA.take_ownership (dir)
-    sys "#{SYSADM} take #{dir}"
-  end
-
-  def SA.kill_user_procs (username)
-    sys "#{SYSADM} kill #{username}"
-  end
-
-  def SA.cleanup_instance_dir (dir)
-    sys "#{SYSADM} rminst #{dir}"
-  end
-
-  def SA.remove_tmp_dir (dir)
-    sys "#{SYSADM} rmtmp #{dir}"
-  end
-
   def SA.create_container (id) 
     `#{SYSADM} runlxc create_container #{id}`.strip # this returns the full directory
   end
@@ -122,67 +102,5 @@ module SA
     myuid = Process.euid
     system("#{SYSADM} runlxc create_staging_dir #{myuid} #{path}")
   end
-
-  # Run a command on host with imposed ulimits.
-  # This is currently only used for running staging process.
-  # TODO: remove this function once we move staging to LXC.
-  def SA.run_as (user, command, opts={}, &block)
-    opts = {:dir => nil, :env => nil, :limits => nil, :timeout => 0}.merge(opts)
-
-    # Timeout process termination:
-    # 1. We can't simply kill all user processes with kill_user_procs(user)
-    #    because this will terminate the app, too. 
-    # 2. Closing the /bin/sh's stdin in hope it would send SIGTERM to its 
-    #    children didn't work, I think because a forked shell is not a session
-    #    owner.
-    # 3. Calling Process.kill(SIG*, pid) doesn't make sense because the process
-    #    belongs to a different user.
-    # Conclusion:
-    #    This should happen from inside of the child process.
-
-    if not "\"'".include? command[0]  # wrap in quotes if necessary
-      command = command.sub("'", "\\\\'")  # escape single quotes
-      command = "'#{command}'"
-    end
-    command = "sh -c #{command}"  # run with `sh -c ...` to allow pipes, quotes, etc..
-
-    exec_operation = proc do |process|
-      # set the minimal usable PATH
-      process.send_data("export PATH=/usr/bin:/bin\n")
-      process.send_data("cd #{opts[:dir]}\n") if opts[:dir]
-      # we want to limit all processes potentially running user code, without exceptions
-      limits = {:mem => ONE_GIG, :fds => 4096, :disk => ONE_GIG}.merge(opts[:limits] || {})
-      # ulimit -m takes kb, soft enforce
-      process.send_data("ulimit -m #{(limits[:mem]/1024).to_i} 2> /dev/null\n")  
-      # virtual memory at 3G, this will be enforced
-      process.send_data("ulimit -v 3000000 2> /dev/null\n") 
-      process.send_data("ulimit -n #{limits[:fds]} 2> /dev/null\n")
-      process.send_data("ulimit -u 512 2> /dev/null\n") # processes/threads
-      # File size to complete disk usage
-      process.send_data("ulimit -f #{limits[:disk]} 2> /dev/null\n") 
-      process.send_data("umask 027\n") # the group is forced to be "stackato"
-      (opts[:env] || {}).each do |k,v| 
-        export_line = "export #{k}=#{v}\n"
-        process.send_data(export_line)
-      end
-
-      # timeout now works with the sigal mask being restored in sysadm
-      timeout = opts[:timeout] || 0
-      command = "timeout #{timeout} #{command}" if timeout > 0
-
-      process.send_data("#{command}\n") 
-      process.send_data("exit\n") # because. (long story)
-      process
-    end
-
-    # Unfortunately, an extra shell is necessary here because there is no 
-    # EM.popen3. This means stderr is merged to the caller's stderr unless
-    # redirected inside the child process. Also, stderr redirection works only
-    # with the three-arg form of EM.system.
-    pid = EM.system("/bin/sh", "-c", "env -i #{SYSADM} runas #{user} 2>&1",
-                    exec_operation, block)
-    pid
-  end
-
 end
 
