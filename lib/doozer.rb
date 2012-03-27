1221febb017e398f091e787cce3b7fa8f032c6d2
Phil Whelan <phil123@gmail.com>
2012-03-27 15:31:11 -0700

    working doozer config updater for CC

--- lib/doozer.rb
+++ lib/doozer.rb
@@ -48,25 +48,25 @@ module Doozer
   end
 
   def self._stash_component_config_value(config, e)
-    path = config
     path_parts = e.path.split("/")
     path_parts.shift # remove empty part before first "/"
     path_parts.shift # remove "proc"
     path_parts.shift # remove component_id
     path_parts.shift # remove "config"
-    # replace dashes with underscores in keys
-    path_parts = path_parts.map{|key| key.gsub(/\-/, '_')}
+    # replace dashes with underscores in keys and make symbols
+    path_parts = path_parts.map{|key| key.gsub(/\-/, '_').to_sym}
     # we won't create a hash for last key
-    last_key = path_parts.pop
+    key = path_parts.pop
     path_parts.each do |part|
       part_sym = part.to_sym
-      if not path.has_key? part_sym
-        path[part_sym] = {}
+      if not config.has_key? part_sym
+        config[part_sym] = {}
       end
-      path = path[part_sym]
+      config = config[part_sym]
     end
-    path[last_key.to_sym] = JSON.load(e.value)
-    return path_parts + [last_key]
+    new_value = JSON.load(e.value)
+    config[key] = new_value
+    return path_parts, key, new_value
   end
 
   def self.watch_component_config(component_id, config, callback=nil)
@@ -77,9 +77,28 @@ module Doozer
         end
         c.rev do |v|
           c.watch(v, File.join(component_config_path(component_id), "**")) do |e, err|
-            path_parts = _stash_component_config_value(config, e)
+            path, key, value = _stash_component_config_value(config, e)
             if callback
-              callback.call(e.path, path_parts, e.value)
+              callback.call(path, key, value)
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def self.set_component_config_value(component_id, path, key, value)
+    EM.next_tick do
+      Fraggle.connect(DEFAULT_URI) do |c, err|
+        if err
+          raise err.message
+        end
+        c.rev do |v|
+          doozer_path = File.join(component_config_path(component_id), (path + [key]).map{|p| p.to_s.gsub(/\_/, '-')})
+          doozer_value = JSON.dump(value)
+          c.set(v, doozer_path, doozer_value) do |e, err|
+            if err
+              raise "Failed to set doozer value " + doozer_path.to_s + " = " + doozer_value.to_s + " " + err.message
             end
           end
         end
