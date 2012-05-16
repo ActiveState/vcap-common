5f67ef80721767fe18d8b711afb9504119e0d3fb
Daniil Kulchenko <daniilk@activestate.com>
2012-05-16 16:27:47 -0700

    runtimes.rb fixes and periodic runtimes refreshing for DEA.

--- lib/vcap/runtimes.rb
+++ lib/vcap/runtimes.rb
@@ -3,25 +3,39 @@ require 'yaml'
 RUNTIMES_LIST = File.join( ENV['HOME'], 'stackato/etc/runtimes.yml' )
 
 module VCAP
-	class Runtimes
-		def self.list_for_cc
-      res = {}
-		  _get[:runtimes].each do |k,v|
-        res[k] = { :version => v[:version] }
-        res[k][:debug_modes] = v[:debug_modes] if v[:debug_modes]
+      class Runtimes
+        def self.list_for_cc
+          res = {}
+          symbolize_keys(_get["runtimes"]).each do |k,v|
+          res[k] = { :version => v[:version] }
+          res[k][:debug_modes] = v[:debug_modes] if v[:debug_modes]
+        end
+
+        res
       end
 
-      res
-		end
+      def self.list
+        _get["runtimes"]
+      end
 
-    def self.list
-      _get[:runtimes]
-    end
-
-		private
-
-		def _get
-		  YAML.load_file(RUNTIMES_LIST)
-		end
+      def self.symbolize_keys(data)
+        case data
+        when Array
+          data.map { |arg| symbolize_keys(arg) }
+        when Hash
+          Hash[
+            data.map { |key, value|
+              k = key.is_a?(String) ? key.to_sym : key
+              v = symbolize_keys(value)
+              [k,v]
+            }]
+	else
+	  data
 	end
+      end
+
+      def self._get
+        YAML.load_file(RUNTIMES_LIST)
+      end
+    end
 end
