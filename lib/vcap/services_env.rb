aed35e3c172af60d403e020daae81e073c56f1f2
Jan Dubois <jand@activestate.com>
2012-07-30 17:11:02 -0700

    Use unversioned hash keys in $STACKATO_SERVICES
    This makes it easier to maintain Stackato itself, and also to write applications that
    don't break every time the Stackato services change a version number.
    
    Fixes http://bugs.activestate.com/show_bug.cgi?id=94905

--- lib/vcap/services_env.rb
+++ lib/vcap/services_env.rb
@@ -25,11 +25,12 @@ module VCAP
         stackato_services[s[:name]] = stackato_hash = {}
 
         vcap_hash = {}
-        vcap_services[s[:label]] ||= []
-        vcap_services[s[:label]] << vcap_hash
+        unversioned_label = s[:label].sub(/-\d+(\.\d+)*$/, "")
+        vcap_services[unversioned_label] ||= []
+        vcap_services[unversioned_label] << vcap_hash
 
         whitelist.each {|k| vcap_hash[k] = s[k] if s[k]}
-        if s[:label].start_with? 'filesystem'
+        if unversioned_label == 'filesystem'
           stackato_hash[:dir] = vcap_hash[:dir] = "/app/fs/#{s[:name]}"
           vcap_hash.delete(:credentials)
         elsif s[:credentials].is_a? Hash
@@ -43,48 +44,48 @@ module VCAP
       # Add individual environment variables for each service type as long
       # as only a single service of that type has been bound.
 
-      only_item(vcap_services['mysql-5.5']) do |s|
+      only_item(vcap_services['mysql']) do |s|
         c = s[:credentials]
         e["MYSQL_URL"] = "mysql://#{c[:user]}:#{c[:password]}@#{c[:host]}:#{c[:port]}/#{c[:name]}"
       end
 
-      only_item(vcap_services['postgresql-9.1']) do |s|
+      only_item(vcap_services['postgresql']) do |s|
         c = s[:credentials]
         e["POSTGRESQL_URL"] = "postgres://#{c[:user]}:#{c[:password]}@#{c[:host]}:#{c[:port]}/#{c[:name]}"
       end
 
       # Store relational database url also in $DATABASE_URL if there is just one database
-      if ((vcap_services['mysql-5.5'] || []) + (vcap_services['postgresql-9.1'] || [])).size == 1
+      if ((vcap_services['mysql'] || []) + (vcap_services['postgresql'] || [])).size == 1
         e["DATABASE_URL"] = e["MYSQL_URL"] || e["POSTGRESQL_URL"]
       end
 
-      only_item(vcap_services['mongodb-1.8']) do |s|
+      only_item(vcap_services['mongodb']) do |s|
         c = s[:credentials]
         e["MONGODB_URL"] = "mongodb://#{c[:username]}:#{c[:password]}@#{c[:host]}:#{c[:port]}/#{c[:db]}"
       end
 
-      only_item(vcap_services['redis-2.2']) do |s|
+      only_item(vcap_services['redis']) do |s|
         c = s[:credentials]
         # redis does not seem to require a 'user' field
         e["REDIS_URL"] = "redis://user:#{c[:password]}@#{c[:host]}:#{c[:port]}/"
       end
 
-      only_item(vcap_services['rabbitmq-2.4']) do |s|
+      only_item(vcap_services['rabbitmq']) do |s|
         c = s[:credentials]
         e["RABBITMQ_URL"] = "amqp://#{c[:username]}:#{c[:password]}@#{c[:host]}:#{c[:port]}/#{c[:vhost]}"
       end
 
-      only_item(vcap_services['memcached-1.4']) do |s|
+      only_item(vcap_services['memcached']) do |s|
         c = s[:credentials]
         e["MEMCACHE_URL"] = "#{c[:host]}:#{c[:port]}"
       end
 
-      only_item(vcap_services['filesystem-1.0']) do |s|
+      only_item(vcap_services['filesystem']) do |s|
         e["STACKATO_FILESYSTEM"] = s[:dir]
       end
 
       # For filesystem services we will also create a specific variable for each one
-      (vcap_services['filesystem-1.0'] || []).each do |s|
+      (vcap_services['filesystem'] || []).each do |s|
         e["STACKATO_FILESYSTEM_#{s[:name].upcase.gsub('-', '_')}"] = s[:dir]
       end
 
