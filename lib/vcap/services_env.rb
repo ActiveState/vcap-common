06efe9edd696841952589f98480df12419440bd3
Jan Dubois <jand@activestate.com>
2013-03-15 16:40:02 -0700

    Add ORACLE_URL when there is a single oracle service configured
    This commit also adds a JDBC_ORACLE_URL variable that contains the
    connection string for th JDB Thin driver, which seems to be more
    commonly used that the format used by the other relational dbs.
    
    I have choosen Thin over OCI because Thin works even without the
    Oracle client libraries on the DEA and seems to have similar performance
    characteristics as OCI.
    
    ORACLE_URL is now also a contender for DATABASE_URL, which will only
    be set if exactly one of MYSQL_URL, POSTGRESQL_URL and ORACLE_URL
    is set.
    
    Ref http://bugs.activestate.com/show_bug.cgi?id=97520

--- lib/vcap/services_env.rb
+++ lib/vcap/services_env.rb
@@ -64,14 +64,20 @@ module VCAP
         e["MYSQL_URL"] = "mysql://#{c[:user]}:#{c[:password]}@#{c[:host]}:#{c[:port]}/#{c[:name]}"
       end
 
+      only_item(vcap_services['oracle']) do |s|
+        c = s[:credentials]
+        e["ORACLE_URL"] = "oracle://#{c[:user]}:#{c[:pass]}@#{c[:host]}:#{c[:port]}/#{c[:name]}"
+        e["JDBC_ORACLE_URL"] = "jdbc:oracle:thin:#{c[:user]}/#{c[:pass]}@//#{c[:host]}:#{c[:port]}/#{c[:name]}"
+      end
+
       only_item(vcap_services['postgresql']) do |s|
         c = s[:credentials]
         e["POSTGRESQL_URL"] = "postgres://#{c[:user]}:#{c[:password]}@#{c[:host]}:#{c[:port]}/#{c[:name]}"
       end
 
       # Store relational database url also in $DATABASE_URL if there is just one database
-      if ((vcap_services['mysql'] || []) + (vcap_services['postgresql'] || [])).size == 1
-        e["DATABASE_URL"] = e["MYSQL_URL"] || e["POSTGRESQL_URL"]
+      if ((vcap_services['mysql'] || []) + (vcap_services['oracle'] || []) + (vcap_services['postgresql'] || [])).size == 1
+        e["DATABASE_URL"] = e["MYSQL_URL"] || e["ORACLE_URL"] || e["POSTGRESQL_URL"]
       end
 
       only_item(vcap_services['mongodb']) do |s|
